2936c6abc21daa8681dc12687062f924c694951e
Josh <joshuarmorrison@gmail.com>
2021-09-11 21:40:34 -0600

    Weather converter

--- app/Controllers/WeatherController.js
+++ app/Controllers/WeatherController.js
@@ -7,14 +7,54 @@ function _drawWeather(){
     document.getElementById("weatherzone").innerHTML = ProxyState.weather.Template
 }
 
+function _tempConverter(temp){
+    let newtemp = Math.round(((temp - 273.15)*1.8)+32)
+    // @ts-ignore
+    ProxyState.weather.temp = newtemp
+    ProxyState.weather = ProxyState.weather
+    
+}
 
 export class WeatherController{
     constructor(){
         ProxyState.on("weather",_drawWeather);
         this.getWeather()
+        _tempConverter()
     }
     async getWeather(){
         await weatherService.getWeather()
-        console.log(ProxyState.weather)
+        
+        // @ts-ignore
+        let temp = ProxyState.weather.temp
+        _tempConverter(temp)
+    }
+    tempConverter(){
+        // @ts-ignore
+        if (ProxyState.weather.type == false){
+            // @ts-ignore
+            ProxyState.weather.type = true
+            ProxyState.weather = ProxyState.weather
+
+            // @ts-ignore
+            let farenheit = ProxyState.weather.temp 
+            let celcius = Math.round(((farenheit-32)*5)/9)
+
+            // @ts-ignore
+            ProxyState.weather.temp = celcius
+            ProxyState.weather = ProxyState.weather
+        // @ts-ignore
+        } else if (ProxyState.weather.type == true){
+            // @ts-ignore
+            ProxyState.weather.type = false
+            ProxyState.weather = ProxyState.weather
+
+            // @ts-ignore
+            let celcius = ProxyState.weather.temp
+            let farenheit = Math.round((celcius * 1.8)+ 32)
+
+            // @ts-ignore
+            ProxyState.weather.temp = farenheit
+            ProxyState.weather = ProxyState.weather
+        }
     }
 }
--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -5,13 +5,14 @@ export class weather{
         this.temp = weatherData.main.temp 
         this.weather = weatherData.weather 
         this.clouds = weatherData.clouds 
+        this.type = weatherData.type || false
 
         
 
     }
     get Template() {
         return /*html*/ `
-        <h5 class="text-white">
+        <h5 class="text-white" onclick="app.weatherController.tempConverter()">
         ${this.temp}
       </h5>
       <h6 class="text-white">${this.name}</h6>`
